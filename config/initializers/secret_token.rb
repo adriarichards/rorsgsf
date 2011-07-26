146d0298f8ae067c4ef13874541fc3660c3520e4
Adria Richards <adriarichards@Adria-Richardss-MacBook-Pro.local>
2011-07-25 18:42:40 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wine::Application.config.secret_token = '53de242259347c32b8f5f97ca6a9571aab5a4748a2df8362d4cad0482462211d7a51e5c08c58793879de00cb547b1b404100019aaf3ded8f82dc4735be90fc31'
